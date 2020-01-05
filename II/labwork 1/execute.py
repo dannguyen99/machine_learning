efce0ba9b9e489ad47046f2d092a9efb671fda47
dannguyen <dannguyen0801@gmail.com>
2020-01-05 23:42:18 +0700

    update machine learning 2

--- II/labwork 1/execute.py
+++ II/labwork 1/execute.py
@@ -1,16 +1,133 @@
-import numpy as np
+import math
+
 import pandas as pd
-from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 # study dataset properties
-abalone_dataset = pd.read_csv('abalone.csv', sep=',')
-iris_dataset = pd.read_csv('iris.csv', sep=',')
+abalone_dataset = pd.read_csv('abalone.csv', sep=',', skipinitialspace=True)
+iris_dataset = pd.read_csv('iris.csv', sep=',', skipinitialspace=True)
+
+print(iris_dataset.corr())
+
+
 # print(abalone_dataset.mean())
 # print(abalone_dataset.var())
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(abalone_dataset.corr())
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+#     print(abalone_dataset.corr())
 # print(abalone_dataset.cov())
 # print(abalone_dataset.corr())
 # print(iris_dataset.describe())
+class Dataset:
+    def __init__(self, datafile_path):
+        self.features_name = None
+        self.target_name = None
+        self.data = pd.read_csv(datafile_path, sep=',', skipinitialspace=True)
+
+    def set_features_name(self, features):
+        self.features_name = features
+
+    def set_target_name(self, target):
+        self.target_name = target
+
+    def standardize_data(self):
+        if self.features_name is None or self.target_name is None:
+            pass
+        y = self.data.loc[:, self.target_name].values
+        StandardScaler().fit_transform(self.data.loc[:, self.features_name].values)
+
+    @staticmethod
+    def mean(serie):
+        return round(sum(serie) / len(serie), 4)
+
+    def mean_all(self):
+        mean_values = []
+        print("attribute \t\t mean")
+        for i in self.data:
+            column = self.data[i]
+            if column.dtype == 'int64' or column.dtype == 'float64':
+                mean_value = self.mean(column)
+                print(i, "\t", mean_value)
+                mean_values.append(mean_value)
+        return mean_values
+
+    def standard_deviation(self, serie):
+        return math.sqrt(self.variance(serie))
+
+    def variance(self, serie):
+        mean_value = self.mean(serie)
+        sum_var = 0
+        for i in serie:
+            sum_var += (mean_value - i) ** 2
+        return round(sum_var / len(serie), 4)
+
+    def variance_all(self):
+        variance_values = []
+        print("attribute \t\t variance")
+        for i in self.data:
+            column = self.data[i]
+            if column.dtype == 'int64' or column.dtype == 'float64':
+                variance_value = self.variance(column)
+                print(i, "\t", variance_value)
+                variance_values.append(variance_value)
+        return variance_values
+
+    def covariance(self, serie_x, serie_y):
+        mean_x = self.mean(serie_x)
+        mean_y = self.mean(serie_y)
+        sum_covariance = 0
+        for x, y in zip(serie_x, serie_y):
+            sum_covariance += (x - mean_x) * (y - mean_y)
+        return round(sum_covariance / len(self.data.index), 3)
+
+    def covariance_all(self):
+        covariance_values = []
+        print("attribute\t\t", end="")
+        for i in self.data:
+            column = self.data[i]
+            if column.dtype == 'int64' or column.dtype == 'float64':
+                print(i, "\t", end="")
+        print()
+        for i in self.data:
+            column = self.data[i]
+            if column.dtype == 'int64' or column.dtype == 'float64':
+                print(i, "\t", end="")
+                for j in self.data:
+                    row = self.data[j]
+                    if row.dtype == 'int64' or row.dtype == 'float64':
+                        covariance_value = self.covariance(column, row)
+                        print(covariance_value, "\t\t\t", end="")
+                        covariance_values.append(covariance_value)
+                print()
+        return covariance_values
+
+    def correlation(self, serie_x, serie_y):
+        std_x = self.standard_deviation(serie_x)
+        std_y = self.standard_deviation(serie_y)
+        return round(self.covariance(serie_x, serie_y) / (std_x * std_y), 3)
+
+    def correlation_all(self):
+        correlation_values = []
+        print("attribute\t\t", end="")
+        for i in self.data:
+            column = self.data[i]
+            if column.dtype == 'int64' or column.dtype == 'float64':
+                print(i, "\t", end="")
+        print()
+        for i in self.data:
+            column = self.data[i]
+            if column.dtype == 'int64' or column.dtype == 'float64':
+                print(i, "\t", end="")
+                for j in self.data:
+                    row = self.data[j]
+                    if row.dtype == 'int64' or row.dtype == 'float64':
+                        correlation_value = self.correlation(column, row)
+                        print(correlation_value, "\t\t\t", end="")
+                        correlation_values.append(correlation_value)
+                print()
+        return correlation_values
 
 
+d = Dataset('abalone.csv')
+print(d.data.describe())
+print(d.data.cov())
+d.covariance_all()
---
+++ II/labwork 1/test.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data"
+# load dataset into Pandas DataFrame
+df = pd.read_csv('abalone.csv', sep=',', skipinitialspace=True)
+# from sklearn.preprocessing import StandardScaler
+
+features = ['length', 'diameter', 'height', 'whole weight', 'shucked weight', 'viscera weight', 'shell weight']
+# Separating out the features
+x = df.loc[:, features].values
+# Separating out the target
+y = df.loc[:, ['rings']].values
+# Standardizing the features# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more
+# options can be specified also print(abalone_dataset.corr()) x = StandardScaler().fit_transform(x)
+
+
+pca = PCA(n_components=3)
+principalComponents = pca.fit_transform(x)
+principalDf = pd.DataFrame(data=principalComponents
+                           , columns=['principal component 1', 'principal component 2', 'principal component 3'])
+
+finalDf = pd.concat([principalDf, df[['rings']]], axis=1)
+# print(finalDf.head())
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlabel('Principal Component 1', fontsize=15)
+ax.set_ylabel('Principal Component 2', fontsize=15)
+ax.set_title('2 component PCA', fontsize=20)
+targets = [0, 10]
+colors = ['r', 'g', 'b']
+for target, color in zip(targets, colors):
+    if target < 10:
+        indicesToKeep = finalDf['rings'] <= 10
+    else:
+        indicesToKeep = finalDf['rings'] > 10
+    # print(finalDf['rings'] > 10)
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c=color
+               , s=20)
+ax.legend(["0 to 10", "10 to 30", "20 to 30"])
+ax.grid()
+print(pca.explained_variance_ratio_)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(finalDf.head())
+# plt.show()
