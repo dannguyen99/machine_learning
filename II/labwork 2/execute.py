15514e24b4cbd3e5fef99066a16b84261fa7efa4
dannguyen <dannguyen0801@gmail.com>
2020-01-15 00:38:06 +0700

    update labwork 2

--- II/labwork 2/execute.py
+++ II/labwork 2/execute.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import datasets
+from sklearn import datasets, metrics
 from matplotlib import pyplot as plt
 
 from sklearn.cluster import AgglomerativeClustering, KMeans
@@ -38,22 +38,22 @@ def plot_dendrogram_all(dataset, dataset_name):
     plt.figure()
     plot_dendrogram(AgglomerativeClustering(distance_threshold=0, n_clusters=None, linkage='single').fit(X),
                     truncate_mode='level', p=3)
-    plt.title('Single Linkage')
+    plt.title('Single Linkage of ' + dataset_name)
     plt.xlabel("The number of points in node (or index of point if no parenthesis).")
     plt.figure()
     plot_dendrogram(AgglomerativeClustering(distance_threshold=0, n_clusters=None, linkage='complete').fit(X),
                     truncate_mode='level', p=3)
-    plt.title('Complete Linkage')
+    plt.title('Complete Linkage of ' + dataset_name)
     plt.xlabel("The number of points in node (or index of point if no parenthesis).")
     plt.figure()
     plot_dendrogram(AgglomerativeClustering(distance_threshold=0, n_clusters=None, linkage='average').fit(X),
                     truncate_mode='level', p=3)
-    plt.title('Average Linkage')
+    plt.title('Average Linkage of ' + dataset_name)
     plt.xlabel("The number of points in node (or index of point if no parenthesis).")
     plt.figure()
     plot_dendrogram(AgglomerativeClustering(distance_threshold=0, n_clusters=None, linkage='ward').fit(X),
                     truncate_mode='level', p=3)
-    plt.title('Ward Linkage')
+    plt.title('Ward Linkage of ' + dataset_name)
     plt.xlabel("The number of points in node (or index of point if no parenthesis).")
 
 
@@ -95,33 +95,119 @@ def visualize_ahc_cluster(dataset, dataset_name, linkage_type='ward', feature_se
     return labels
 
 
-def main(show_plot=False):
-    # x = np.array([[1], [2], [9], [12], [20]])
-    # plot the dendrograms of single array
-    # plot_dendrogram_single(x)
+def visualize_elbow_method(dataset, dataset_name):
+    wcss = []
+    X = dataset.data
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters=i, init='random', n_init=10)
+        kmeans.fit(X)
+        wcss.append(kmeans.inertia_)
+    plt.figure()
+    plt.plot(range(1, 11), wcss)
+    plt.title('Elbow Method for ' + dataset_name)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
 
+
+def visualize_kmeans_cluster(dataset, dataset_name, feature_set=None, use_PCA=False):
+    if feature_set is None:
+        feature_set = [0, 1]
+    X = dataset.data
+    Y = dataset.target
+    no_cluster = len(dataset.target_names)
+    feature_names = dataset.feature_names
+    first, second = feature_set[0], feature_set[1]
+    model = KMeans(n_clusters=no_cluster, init='random', n_init=10)
+    model.fit(X)
+    labels = model.labels_
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8))
+    title = dataset_name + " Dataset with k = " + str(no_cluster) + " clusters"
+    fig.suptitle(title, fontsize=18)
+    colors = ['r', 'g', 'b']
+    for i, color in zip(range(no_cluster), colors):
+        ax1.scatter(X[Y == i, first], X[Y == i, second], s=50, marker='o', color=color)
+    ax1.set_title('Actual')
+    for i, color in zip(range(no_cluster), colors):
+        ax2.scatter(X[labels == i, first], X[labels == i, second], s=50, marker='o', cmap='jet')
+    ax2.set_title('Predicted')
+    ax1.set_xlabel(feature_names[first])
+    ax1.set_ylabel(feature_names[second])
+    ax2.set_xlabel(feature_names[first])
+    ax2.set_ylabel(feature_names[second])
+    ax1.legend(dataset.target_names)
+    return labels
+
+
+def clustering_evaluate(dataset, dataset_name, is_print=True):
+    results = {}
+    X = dataset.data
+    no_cluster = len(dataset.target_names)
+    model = KMeans(n_clusters=no_cluster, init='random', n_init=10)
+    model.fit(X)
+    labels_true = dataset.target
+    labels_pred = model.labels_
+    ACC = metrics.accuracy_score(labels_true, labels_pred)
+    results['ACC'] = ACC
+    ARI = metrics.adjusted_rand_score(labels_true, labels_pred)
+    results['ARI'] = ARI
+    MI = metrics.mutual_info_score(labels_true, labels_pred)
+    results['MI'] = MI
+    NMI = metrics.normalized_mutual_info_score(labels_true, labels_pred)
+    results['NMI'] = NMI
+    JC = metrics.jaccard_score(labels_true, labels_pred, average=None)
+    results['JC'] = JC
+    if is_print:
+        print("Evaluating", dataset_name)
+        print("Accuracy =", ACC)
+        print("Adjusted Rand index =", ARI)
+        print("Mutual Information =", MI)
+        print("Normalized Mutual Information =", NMI)
+        print("Jaccard Score = ", JC)
+        print()
+    return results
+
+
+def main(show_plot=False):
+    # 1.1 & 1.2
+    # plot the dendrograms of single array
+    x = np.array([[1], [2], [9], [12], [20]])
+    plot_dendrogram_single(x)
+
+    # 1.3
     # load Iris dataset
     iris = datasets.load_iris()
     iris_name = "Iris"
-
-    # plot the dendrograms of Iris
-    # plot_dendrogram_all(dataset=iris, dataset_name=iris_name)
-
-    # apply ahc with Iris, compare with actual data
-    # visualize_ahc_cluster(dataset=iris, dataset_name=iris_name, linkage_type='single', feature_set=[0, 1])
-
     # load Wine Dataset
     wine = datasets.load_wine()
     wine_name = "Wine"
-
+    # plot the dendrograms of Iris
+    plot_dendrogram_all(dataset=iris, dataset_name=iris_name)
     # plot the dendrograms of Wine
-    # plot_dendrogram_all(dataset=wine, dataset_name=wine_name)
-
+    plot_dendrogram_all(dataset=wine, dataset_name=wine_name)
+    # apply ahc with Iris, compare with actual data
+    visualize_ahc_cluster(dataset=iris, dataset_name=iris_name, linkage_type='complete', feature_set=[0, 1])
     # apply ahc with Wine, compare with actual data
-    # visualize_ahc_cluster(dataset=wine, dataset_name=wine_name, linkage_type='single', feature_set=[0, 1])
+    visualize_ahc_cluster(dataset=wine, dataset_name=wine_name, linkage_type='single', feature_set=[0, 1])
+
+    # 2.1 & 2.2
+    # apply kmeans with Iris, compare with actual data
+    visualize_kmeans_cluster(dataset=iris, dataset_name=iris_name, feature_set=[0, 1])
+    # apply kmeans with Wine, compare with actual data
+    visualize_kmeans_cluster(dataset=wine, dataset_name=wine_name, feature_set=[0, 1])
+
+    # 2.3
+    # using elbow method to determine best number of cluster
+    visualize_elbow_method(iris, iris_name)
+    # using elbow method to determine best number of cluster
+    visualize_elbow_method(wine, wine_name)
+
+    # 2.4
+    # Evaluate Iris dataset
+    clustering_evaluate(dataset=iris, dataset_name=iris_name, is_print=True)
+    # Evaluate Wine dataset
+    clustering_evaluate(dataset=wine, dataset_name=wine_name, is_print=True)
 
     # show the plots
-
     if show_plot:
         plt.show()
 
